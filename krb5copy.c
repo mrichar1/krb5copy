9a65ca21179bb1b7713eac9ec5341377a1aac494
Matthew Richardson <M.Richardson@ed.ac.uk>
2025-02-27 12:01:00 +0000

    Fix up error handling and logging.

--- krb5copy.c
+++ krb5copy.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <krb5.h>
 
 // Global krb5 context
@@ -8,9 +9,14 @@ krb5_ccache src_ccache, tgt_ccache, write_ccache;
 krb5_creds creds;
 krb5_error_code ret;
 krb5_cc_cursor cursor;
-krb5_principal principal;
+krb5_principal src_principal;
+krb5_principal tgt_principal;
 
 int cleanup(int retval) {
+    if (creds.client != NULL) {
+        krb5_free_cred_contents(context, &creds);
+    }
+
     if (src_ccache != NULL) {
         krb5_cc_close(context, src_ccache);
     }
@@ -40,8 +46,6 @@ int log_krb5_error(const char *source, krb5_error_code ret) {
 
 int main() {
 
-    char *parsed_principal = NULL;
-
     // Initialize the Kerberos library
     log_krb5_error(
         "krb5_init_context",
@@ -54,9 +58,11 @@ int main() {
 
     const char *tgt_cache_name = getenv("KRB5CCNEW");
     if (tgt_cache_name == NULL) {
-        fprintf(stderr, "KRB5CCNEW is not set in the environment.\n");
-        return 1;
+        fprintf(stderr, "ERROR: KRB5CCNEW is undefined - target cache is required.\n");
+        cleanup(1);
     }
+
+    fprintf(stderr, "Target cache: %s\n", tgt_cache_name);
 
     // Try to open the source cache
     log_krb5_error(
@@ -67,7 +73,7 @@ int main() {
     // Fail if src cache has no principals
     log_krb5_error(
         "ERROR: No principal found in source cache.",
-        krb5_cc_get_principal(context, src_ccache, &principal)
+        krb5_cc_get_principal(context, src_ccache, &src_principal)
     );
 
     // Open the target cache
@@ -78,11 +84,13 @@ int main() {
 
     // Get target cache type
     const char *tgt_type = krb5_cc_get_type(context, tgt_ccache);
-    fprintf(stderr, "Target cache type: %s\n", tgt_type);
 
     // Check if target already has a principal in it
-    krb5_cc_get_principal(context, tgt_ccache, &principal);
-    krb5_data princ_data = principal->data[0];
+    krb5_cc_get_principal(context, tgt_ccache, &tgt_principal);
+    krb5_data tgt_princ_data = {0};
+    if (tgt_principal != NULL && tgt_principal->data != NULL) {
+        tgt_princ_data = tgt_principal->data[0];
+    }
 
     // Initialize cursor for sequence
     log_krb5_error(
@@ -94,25 +102,25 @@ int main() {
     write_ccache = tgt_ccache;
 
     // Protect existing principal
-    if (princ_data.data != NULL) {
-        fprintf(stderr, "Existing Target cache Principal: %s\n", princ_data.data);
+    if (tgt_princ_data.data != NULL) {
+        fprintf(stderr, "Existing Target cache Principal: %s\n", tgt_princ_data.data);
 
-        if (tgt_type == "FILE") {
+        // FIXME: Add a way to force overwrites
+        if (strcmp(tgt_type, "FILE") == 0) {
             fprintf(stderr, "Cache is of type FILE and contains a principal. Aborting.");
             cleanup(1);
         }
 
-
-        // Target is a collection, so generate a new unique cache inside it...
+        // Target is a collection, so generate a new unique cache inside it.
         krb5_cc_set_default_name(context, tgt_cache_name);
         krb5_cc_new_unique(context, tgt_type, "", &write_ccache);
-
     }
 
-    // Iterate over creds in src cache,
-    while (krb5_cc_next_cred(context, src_ccache, &cursor, &creds) == 0) {
-        krb5_principal princ = creds.client;
-        // fIXME: handle errors from these with ret/log
+    // Iterate over creds in src cache, writing to target
+    while ((ret = krb5_cc_next_cred(context, src_ccache, &cursor, &creds)) == 0) {
+        char *princ_name = NULL;
+        krb5_unparse_name(context, creds.client, &princ_name);
+        fprintf(stderr, "Copying credential: %s\n", princ_name);
         log_krb5_error(
             "krb5_cc_initialize",
             krb5_cc_initialize(context, write_ccache, creds.client)
